aeb7437dbed8a371e474d67ba7377ac2f7b94d69
Derek Murphy <derekmurphy1993@gmail.com>
2020-01-30 10:53:38 -0500

    Get Favorites and Create Favorites
    Get Favorites
    Create Favorites operational

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import { Route } from 'react-router-dom'
 
-import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
-import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
-import Header from '../Header/Header'
-import SignUp from '../SignUp/SignUp'
-import SignIn from '../SignIn/SignIn'
-import SignOut from '../SignOut/SignOut'
-import ChangePassword from '../ChangePassword/ChangePassword'
+import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
+import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
+import Header from './components/Header/Header'
+import SignUp from './components/SignUp/SignUp'
+import SignIn from './components/SignIn/SignIn'
+import SignOut from './components/SignOut/SignOut'
+import ChangePassword from './components/ChangePassword/ChangePassword'
+
+import Favorites from './components/Stops/Favorites'
+import FavesCreate from './components/Stops/FavoritesCreate'
+import Favorite from './components/Stops/Favorite'
 
 class App extends Component {
   constructor () {
@@ -42,6 +46,13 @@ class App extends Component {
           />
         ))}
         <main className="container">
+          <Route exact path='/favorites/:id' render={(props) => (
+            <Favorite user={user} match={props.match} history={props.history} alert={this.alert} />
+          )} />
+          <Route exact path="/favorites" component={Favorites}/>
+          <AuthenticatedRoute user={user} path='/create-Favorite' render={() => (
+            <FavesCreate alert={this.alert} user={user} />
+          )} />
           <Route path='/sign-up' render={() => (
             <SignUp alert={this.alert} setUser={this.setUser} />
           )} />
---
+++ src/components/Stops/Favorites.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+// import { Link } from 'react-router-dom'
+// When you use the ListGroup with the action prop
+// You can skip the Link component from react-router
+import ListGroup from 'react-bootstrap/ListGroup'
+
+class Favorites extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      favorites: []
+    }
+  }
+
+  componentDidMount () {
+    this.getFaves()
+  }
+
+  componentDidUpdate () {
+    // We can check if the component from the previous
+    // route sent us some data using the location prop.
+    // For convenience, we're destructing the state
+    // property from the location object.  For clarity,
+    // (so we don't confuse it with the component's state)
+    // we're renaming it to locationState when we destructure.
+    const { state: locationState } = this.props.location
+
+    // Now we check if the locationState is undefined
+    // and if it isn't, check if it has a deleted property.
+    if (locationState && locationState.deleted) {
+      // Use the book id passed in the deleted property
+      // to find out of there's actually a book to remove
+      // from state so that we don't fall into an infinite loop.
+      const favesToRemove = this.state.favorites.find(favorite => favorite.id === locationState.deleted)
+      if (favesToRemove) {
+        // Only run getBooks if the state still contains
+        // a deleted book.
+        this.getFaves()
+      }
+    }
+  }
+
+  getFaves = () => {
+    axios({
+      url: 'http://localhost:4741/favorites',
+      method: 'GET'
+    })
+      .then(response => {
+        this.setState({ favorites: response.data.favorites.reverse() })
+      })
+      .catch(console.error)
+  }
+
+  render () {
+    let favesJsx = ''
+    // If the books array length is 0 (falsy)
+    // then ! (not) falsy is (truthy) so display
+    // a Loading message.  This is a good place for a
+    // spinner. Check out these from react-bootstrap:
+    // https://react-bootstrap.github.io/components/spinners/
+    if (!this.state.favorites.length) {
+      favesJsx = <p>Loading...</p>
+    } else {
+      favesJsx = this.state.favorites.map(favorite => (
+        <ListGroup.Item
+          key={favorite.id}
+          action
+          href={`#/favorites/${favorite.id}`}
+          className="d-flex justify-content-between"
+        >
+          <span>{favorite.description}</span>
+          <span>{favorite.station.name}</span>
+          <span>{favorite.station.url_path}</span>
+        </ListGroup.Item>
+
+      ))
+    }
+
+    // If you used just an li element above be sure to
+    // substitute a ul element for the ListGroup below:
+    return (
+      <div className="col-sm-10 col-md-8 mx-auto mt-5">
+        <h2>Favorites</h2>
+        <ListGroup>
+          {favesJsx}
+        </ListGroup>
+      </div>
+    )
+  }
+}
+
+export default Favorites
